67c8eb8fde54c2605a844798bd6a7aa000e71149
xuhr1 <xuhr1@yonyou.com>
2017-09-07 20:27:23 +0800

    已经完成 RPC server 完成 Rpc报文设计 完成 Rpc 序列化和反序列化设计 计划今天完成 RPC 解码编码

--- xFastRpc-common/src/main/java/com/xhr/xFastRpc/common/codecookker/RpcEncoder.java
+++ xFastRpc-common/src/main/java/com/xhr/xFastRpc/common/codecookker/RpcEncoder.java
@@ -1,9 +1,31 @@
 package com.xhr.xFastRpc.common.codecookker;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import org.apache.commons.lang3.SerializationUtils;
+
+
 /**
+ * Rpc 编码器
  * @author 徐浩然
  * @version RpcEncoder, 2017-09-07
  */
-public class RpcEncoder
+public class RpcEncoder extends MessageToByteEncoder
 {
+    private  Class<?> genericClass;
+
+    public  RpcEncoder(Class<?> genericClass) { this.genericClass = genericClass;}
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception
+    {
+        if (genericClass.isInstance(msg))
+        {
+            byte[] data = SerializationUtil.serialize(msg);
+            out.writeInt(data.length);
+            out.writeBytes(data);
+        }
+
+    }
 }
---
+++ xFastRpc-common/src/main/java/com/xhr/xFastRpc/common/utils/SerializationUtil.java
@@ -0,0 +1,82 @@
+package com.xhr.xFastRpc.common.utils;
+
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtobufIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.dyuproject.protostuff.runtime.RuntimeSchema.getSchema;
+
+/**
+ * 序列化工具
+ * （ Protostuff ）
+ * @author 徐浩然
+ * @version SerializationUtil, 2017-09-07
+ */
+public class SerializationUtil
+{
+    private  static Map<Class<?>,Schema<?>> cachedSchema = new ConcurrentHashMap<>();
+
+    private  static Objenesis objenesis = new ObjenesisStd(true);
+
+    public SerializationUtil()
+    {
+    }
+    /* 序列化
+    对象-数组*/
+
+    @SuppressWarnings("unchecked")
+    public static <T> byte[] serialize(T obj){
+        //获得对象的类
+        Class<T> cls =(Class<T>) obj.getClass();
+        //使用LinkedBuffer分配一块默认大小的buffer空间
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        try
+        {
+            //通过对象的类构建对应的schema
+            Schema<T> schema = getSchema(cls);
+            //使用给定的schema将对象序列化为一个byte数组，并返回
+            return ProtobufIOUtil.toByteArray(obj,schema,buffer);
+        }catch (Exception e){
+            throw new IllegalStateException(e.getMessage(),e);
+        } finally
+        {
+            buffer.clear();
+        }
+    }
+
+    /*反序列化 字节- 对象*/
+    @SuppressWarnings("unchecked")
+    public static <T> T deserialize(byte[] data , Class<T> cls){
+        try
+        {   //使用objenesis实例化一个类的对象
+            T message = objenesis.newInstance(cls);
+            //通过对象的类构建对应的schema
+            Schema<T> schema =getSchema(cls);
+            //使用给定的schema将byte数组和对象合并，并返回
+            ProtobufIOUtil.mergeFrom(data,message,schema);
+            return  message;
+        }catch (Exception e){
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    //优化  使用缓存 类对应的Schema可以被缓存起来
+    @SuppressWarnings("unchecked")
+    private static <T> Schema<T> getSchema(Class<T> cls) {
+        Schema<T> schema = (Schema<T>) cachedSchema.get(cls);
+        if (schema == null) {
+            //todo RuntimeSchema 用法需要学习一下
+            schema = RuntimeSchema.createFrom(cls);
+            cachedSchema.put(cls, schema);
+        }
+        return schema;
+    }
+
+
+}
